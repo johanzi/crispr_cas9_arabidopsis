d445c980f892ae90da8fd633c4b8753ab683d634
Johan Zicola <johan.zicola@gmail.com>
2019-01-14 22:20:28 +0100

    Implement fasta to genbank conversion and the addition of features

--- find_pam.py
+++ find_pam.py
@@ -4,7 +4,7 @@
 import os
 import sys
 import argparse
-from Bio import SeqIO
+from Bio import SeqIO, SeqFeature
 from Bio.Alphabet import generic_dna, generic_protein
 
 '''
@@ -44,14 +44,48 @@ sequences = list(SeqIO.parse(input_handle, "fasta"))
 
 if len(sequences) > 1:
 	sys.exit("The input fasta file contains "+str(len(sequences))+". I should contain only one sequence")
+else:
+    sequence = sequences[0]
 
-sequences[0].seq.alphabet = generic_dna
+# Get DNA sequence from the fasta file
+sequence.seq.alphabet = generic_dna
 
-count = SeqIO.write(sequences, output_handle, "genbank")
+# Add features before converting to genbank
+
+# Function to generate a sequence
+def create_feature(sequence, name, start, end):
+    if str(name) and int(start) and int(end):
+        my_feature_location = SeqFeature.FeatureLocation(start, end)
+        my_feature = SeqFeature.SeqFeature(my_feature_location,type=name)
+        sequence.features.append(my_feature)
+
+create_feature(sequence, "flop", 10,20)
+create_feature(sequence, "jesus", 100,200)
+create_feature(sequence, "glup", 20,40)
+
+
+print(sequence.features)
+
+# Convert fasta file into genebank file. Not
+# Note that conversion to genbank format leads to the addition of 1 for 
+# each start of features. Therefore, the start position should be given 
+# 0-based system to anticipate the 1-based conversion
+SeqIO.write(sequences, output_handle, "genbank")
+
+
+# Find PAM in DNA sequence
+# https://biopython.org/wiki/Seq
+print(sequences[0].seq.find("ACAAGT"))
 
 output_handle.close()
 input_handle.close()
 
-print("Coverted %i records" % count)
+# Get positions of the PAM and corresponding protospacers
+# The genbank format is 1-based, closed [start, end], range = end - start + 1
+
+
+
+
+
 
 
